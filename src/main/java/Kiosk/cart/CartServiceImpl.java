28e170bf23a3897aa6f6b5b4d04aa29699cc46e0
최대현 <eogus0911@gmail.com>
2024-11-26 20:11:52 +0900

    Fix: cart menu fixed
    장바구니 메뉴 삭제 후 잘못된 에러 메세지 출력되던 문제 수정
    장바구니 메뉴 삭제 후 비어있다면 메인 메뉴로 이동하도록 변경

--- src/main/java/Kiosk/cart/CartServiceImpl.java
+++ src/main/java/Kiosk/cart/CartServiceImpl.java
@@ -10,7 +10,7 @@ public class CartServiceImpl implements CartService {
     @Override
     public CartItem getCartItem(MenuItem item, int quantity) {
         CartItem cartItem = new CartItem(item.getName(), quantity, item.getPrice());
-        System.out.println("");
+        System.out.println();
         System.out.println("\u001B[36m선택한 메뉴\u001B[0m: " + cartItem.getName() + " | " + cartItem.getQuantity() + "개 | 합계: "+ cartItem.getPrice() + "원");
         return cartItem;
     }
@@ -47,7 +47,6 @@ public class CartServiceImpl implements CartService {
             for (CartItem item : cart) {
                 sum += item.getPrice();
             }
-            System.out.println("합계: " + "\u001B[36m" + sum + "\u001B[0m" + "원");
         } else {
             System.out.println("장바구니가 비었습니다.");
         }
